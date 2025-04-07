d0e40a375930570b5ba8bf07d476d4f7eb4076b8
jeanamukwatse <jeanamukwatse@gmail.com>
2025-04-07 17:12:39 +0300

    Add NGSA project

---
+++ app/src/main/java/lg/apps/ngsa/ngsaandroid/MainActivity.kt
@@ -0,0 +1,59 @@
+package lg.apps.ngsa.ngsaandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import lg.apps.ngsa.ngsaandroid.ui.theme.NGSAAndroidTheme
+import android.app.Application
+import android.util.Log
+
+
+class NGSAAndroid : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // App-wide initialization logic here
+        Log.d("NGSAAndroid", " NGSA Android Application started")
+    }
+}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NGSAAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NGSAAndroidTheme {
+        Greeting("Android")
+    }
+}
